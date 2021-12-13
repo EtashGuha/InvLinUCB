3ae64eb2960d571797916e0f04325ac441d0c178
Etash Guha <etashthebomb@gmail.com>
2021-12-12 23:12:51 -0500

    fixed sigma

--- oracle.py
+++ oracle.py
@@ -2,14 +2,15 @@ import numpy as np
 from scipy.stats import truncnorm
 
 class Oracle:
-    def __init__(self, theta, upper=1, lower=0):
+    def __init__(self, theta, upper=1, lower=0, sigma=0.2):
         self.theta = theta
         self.upper = upper
         self.lower = lower
-        
+        self.sigma = sigma
+
     def compute_reward(self, action):
         mu = np.inner(action, self.theta)
-        sigma = .2
+        sigma = self.sigma
         X = truncnorm((self.lower - mu) / sigma, (self.upper - mu) / sigma, loc=mu, scale=sigma)
         return X.rvs(1)[0]
 

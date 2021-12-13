0d1a180161fb9d32182c9882f31771a6af123a48
Jim James <github@jimjam.es>
2021-12-12 21:29:48 -0600

    Change test to run with battery data

--- oracle.py
+++ oracle.py
@@ -2,15 +2,15 @@ import numpy as np
 from scipy.stats import truncnorm
 
 class Oracle:
-    def __init__(self, theta, upper=1, lower=0):
+    def __init__(self, theta, upper=1, lower=0, sigma=0.2):
         self.theta = theta
         self.upper = upper
         self.lower = lower
+        self.sigma = sigma
         
     def compute_reward(self, action):
         mu = np.inner(action, self.theta)
-        sigma = .2
-        X = truncnorm((self.lower - mu) / sigma, (self.upper - mu) / sigma, loc=mu, scale=sigma)
+        X = truncnorm((self.lower - mu) / self.sigma, (self.upper - mu) / self.sigma, loc=mu, scale=self.sigma)
         return X.rvs(1)[0]
 
 def calc_pseudoregret(theta, actions):
